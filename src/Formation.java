c2ddf1412f003ae5d1144bfbc69b196faa1e4b8a
nino.arcelin@gmail.com <101625357+Nin0A@users.noreply.github.com>
2023-09-26 15:16:45 +0200

    ajout de la classe Formation

---
+++ src/Formation.java
@@ -0,0 +1,75 @@
+import java.util.HashMap;
+import java.util.Objects;
+
+/**
+ * Classe formation : représente une formation (idFormation : identifiant de la formation, listeMatiere : HashMap<matiere -> coeff> )
+ */
+public class Formation extends HashMap{
+    /**
+     * déclarations des attributs
+     */
+    private String idFormation;
+    private HashMap listeMatiere;
+
+    /**
+     * Constructeur Vide
+     */
+    Formation(String id){
+        this.idFormation =id;
+        this.listeMatiere =new HashMap<String, Double>();
+    }
+
+    /**
+     * méthode equals : compare l'élement courant à celui en paramètre et vérifie si ce sont les mêmes
+     * @param o object to be compared for equality with this map
+     * @return
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Formation formation = (Formation) o;
+        return Objects.equals(idFormation, formation.idFormation) && Objects.equals(listeMatiere, formation.listeMatiere);
+    }
+
+    /**
+     * méthode hashCode : indexe les key
+     * @return
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), idFormation, listeMatiere);
+    }
+
+    /**
+     * méthode add : ajoute une matiere -> coefficient
+     * @param matiere
+     * @param coeff
+     */
+    public void add(String matiere, double coeff){
+        if(matiere!=null && coeff >0)
+            this.listeMatiere.put(matiere,coeff);
+    }
+
+    /**
+     * méthode delete : permet de supprimer une maitere -> coefficient à la formation
+     * @param matiere
+     */
+    public void delete(String matiere){
+        if(this.listeMatiere !=null && this.listeMatiere.containsKey(matiere))
+            this.listeMatiere.remove(matiere);
+    }
+
+    /**
+     * méthode get : return le coefficient de la matière en paramètre
+     * @param matiere
+     * @return
+     */
+    public double get(String matiere){
+        double res = -1;
+        if(matiere!=null && this.listeMatiere.containsKey(matiere))
+            res= (double)this.listeMatiere.get(matiere);
+        return res;
+    }
+}
